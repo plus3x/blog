968da67ee5e357c24643dddd4fb76a3a715527ea
Vladislav Petrov <electronicchest@gmail.com>
2013-12-23 16:25:10 +0400

    Add categories to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :delete_all
+  belongs_to :category, touch: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223114948) do
+ActiveRecord::Schema.define(version: 20131223120159) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "description"
@@ -31,6 +37,9 @@ ActiveRecord::Schema.define(version: 20131223114948) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author_ip"
+    t.integer  "category_id"
   end
+
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
 
 end
